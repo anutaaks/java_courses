a4249f473c94e9197258d8b3741291408492c3af
anutaaks <gannaaksonova@Gannas-MacBook-Air.local>
2018-04-10 23:37:00 +0200

    task_2( second part)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -11,6 +11,11 @@ public class Point {
     this.x2 = x2;
     this.y1 = y1;
     this.y2 = y2;
+
 }
+
+    public double distance() {
+        return Math.sqrt((this.x2 - this.x1) * (this.x2 - this.x1) + (this.y2 - this.y1) * (this.y2 - this.y1));
+    }
     }
 
