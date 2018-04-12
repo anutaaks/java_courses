89aa8545753a2cf6273203772d74fecf66784890
anutaaks <gannaaksonova@Gannas-MacBook-Air.local>
2018-04-12 21:52:55 +0200

    rework_task2_part1

--- sandbox/src/main/java/ru/stqa/pft/sandbox/CalculateDistance.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/CalculateDistance.java
@@ -3,11 +3,14 @@ package ru.stqa.pft.sandbox;
 public class CalculateDistance {
 
     public static void main(String[] args) {
-        Point p = new Point(2,5,3,7);
+        Point p1 = new Point(2,5);
+        Point p2 = new Point (4,7);
 
-        System.out.println("Distance between points" + "=" + p.distance());
+        System.out.println("Distance between points" + "=" + distance(p1,p2));
 
     }
-
+    public static double distance(Point p1, Point p2){
+        return Math.sqrt((p2.x - p1.x)*(p2.x - p1.x) + (p2.y - p1.y)*(p2.y - p1.y));
+    }
 
 }
