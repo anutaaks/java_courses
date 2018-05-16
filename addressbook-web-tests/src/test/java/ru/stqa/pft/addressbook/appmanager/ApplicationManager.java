8904db2299213939d45a8d5361d4d392bb1010cc
anutaaks <gannaaksonova@Gannas-MacBook-Air.local>
2018-05-16 21:50:33 +0200

    add drivers and change drivers manager

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,22 +1,33 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
+import static org.openqa.selenium.remote.BrowserType.*;
 
 public class ApplicationManager {
 
-    FirefoxDriver wd;
+    WebDriver wd;
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
     private SessionHelper sessionHelper;
+    private String browser;
 
-
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
-      wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+        if (browser.equals(BrowserType.FIREFOX)){
+            wd = new FirefoxDriver();
+        }else if (browser.equals(BrowserType.CHROME)) {
+            wd = new ChromeDriver();
+        }
       wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
       wd.get("http://localhost/addressbook/index.php");
         groupHelper = new GroupHelper(wd);
